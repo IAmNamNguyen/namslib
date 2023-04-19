115b8524b884f6b2fe4a9f8ac20720207b018ba5
IAmNamNguyen <124715034+IAmNamNguyen@users.noreply.github.com>
2023-04-19 22:29:14 +0700

    Update toanhoc.py

--- toanhoc.py
+++ toanhoc.py
@@ -32,3 +32,27 @@ def nhithucnewton(a, b, n):
         k = k - 1
         h = h + 1
     return f'Khai triển: {pheptinh}, kết quả lần lượt là: {ketqua}'
+def kiemtrasonguyento(so):
+    if so == 2:
+        return f"{so} là số nguyên tố"
+    elif so < 2:
+        return f"{so} không phải là số nguyên tố"
+    elif so % 2 == 0:
+        return f"{so} không phải là số nguyên tố"
+    else: 
+        for i in range(3,int(so**0.5)+1,2):
+            if so % i == 0:
+                return f"{so} không phải là số nguyên tố"
+            else:
+                return f"{so} là số nguyên tố"
+def lietkesonguyento(sogioihan):
+    danhsach = []
+    for so in range(2,sogioihan+1):
+        if so == 2:
+            danhsach.append(so)
+        for i in range(3,int(so**0.5)+1,2):
+            if so % i == 0:
+                break
+        else:
+            danhsach.append(so)
+    return danhsach
